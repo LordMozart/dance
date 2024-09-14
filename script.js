840e3e0976e100b2883a09064451adfdd3f7b0f4
Jake <161117028+LordMozart@users.noreply.github.com>
2024-09-14 18:37:36 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 audioElement.play();
+
+window.onload = setInterval(GameLoop, 1000 / 10); //10fps
+
+let myAudio = new Audio();
+
+myAudio.src = '/MyFolder/MyFile.mp3';
+
+function Gameloop() {
+    myAudio.play();
+    if (myAudio.paused == true) {
+        myAudio.play();
+    }
+}
